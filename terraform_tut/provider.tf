c5a4fbbe2bd5127d152fe57cf440855d29886145
Jon-Martin Storm <jonmartin.storm@gmail.com>
2025-03-22 13:18:35 +0100

    provider ferdig satt opp

--- terraform_tut/provider.tf
+++ terraform_tut/provider.tf
@@ -18,7 +18,15 @@ variable "proxmox_api_token_id" {
   sensitive = true
 }
 
-variable "proxmox_api_token_secreet" {
+variable "proxmox_api_token_secret" {
   type      = string
   sensitive = true
+}
+
+provider "proxmox" {
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+
+  pm_tls_insecure = true
 }
